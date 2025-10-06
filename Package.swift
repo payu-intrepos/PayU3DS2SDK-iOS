e297b78def8fc17775de6c77d9b051854b50758f
mobilepayu <payumoneyapp@payumoney.com>
2025-10-06 13:28:01 +0530

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -32,7 +32,6 @@ let package = Package(
         .target(
             name: "PayUIndia-3DS2-SDKTarget",
             dependencies: [
-                "PayUTridentityMFAKit",
                 "PayU3DS2CoreKit",
                 "PayU3DS2Kit",
                 .product(
@@ -45,10 +44,6 @@ let package = Package(
                 )
             ],
             path: "PayUIndia-3DS2-SDKWrapper"
-        ),
-        .binaryTarget(
-            name: "PayUTridentityMFAKit",
-            path: "./PayUTridentityMFAKit.xcframework"
         ),
         .binaryTarget(
             name: "PayU3DS2CoreKit",
