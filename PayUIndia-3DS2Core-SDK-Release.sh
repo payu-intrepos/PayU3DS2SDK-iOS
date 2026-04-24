74393ee6a973e6a81c23e59505972d3a7b087f53
mobilepayu <payumoneyapp@payumoney.com>
2026-04-24 10:24:28 +0530

    Updated frameworks

--- PayUIndia-3DS2Core-SDK-Release.sh
+++ PayUIndia-3DS2Core-SDK-Release.sh
@@ -50,7 +50,7 @@ podTrunkPush() {
 }
 
 podName="PayUIndia-3DS2Core-SDK"
-podVersion="3.0.0"
+podVersion="3.1.0"
 
 podSpec="${podName}.podspec"
 tag="${podName}${podVersion}"
--- PayUIndia-TridentityMFA-SDK-Release.sh
+++ PayUIndia-TridentityMFA-SDK-Release.sh
@@ -50,7 +50,7 @@ podTrunkPush() {
 }
 
 podName="PayUIndia-TridentityMFA-SDK"
-podVersion="1.1.0"
+podVersion="1.2.0"
 
 podSpec="${podName}.podspec"
 tag="${podName}${podVersion}"
