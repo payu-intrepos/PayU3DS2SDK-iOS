e4548920682904b53f1de1d171dfd803e9b732fb
Shubham Garg <Shubham.garg@payu.in>
2026-08-26 14:10:47 +0530

    updated versions

--- PayUIndia-3DS2-SDK-Release.sh
+++ PayUIndia-3DS2-SDK-Release.sh
@@ -50,7 +50,7 @@ podTrunkPush() {
 }
 
 podName="PayUIndia-3DS2-SDK"
-podVersion="5.0.0"
+podVersion="5.0.1"
 
 podSpec="${podName}.podspec"
 tag="${podVersion}"
--- PayUIndia-TridentityMFA-SDK-Release.sh
+++ PayUIndia-TridentityMFA-SDK-Release.sh
@@ -50,7 +50,7 @@ podTrunkPush() {
 }
 
 podName="PayUIndia-TridentityMFA-SDK"
-podVersion="2.0.0"
+podVersion="2.0.1"
 
 podSpec="${podName}.podspec"
 tag="${podName}${podVersion}"
